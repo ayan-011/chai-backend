aeb9e1fd0b9a3c136b96e8ffb14c1d5a6c96f60f
Your Name <ayansaifi2nd@gmail.com>
2024-10-13 00:25:18 +0530

    all setup configred

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,7 +1,7 @@
 // 01.     PROMISE METHOD
 
 const asyncHandler = (requestHandler)=>{
-    (req, res, next) =>{
+   return (req, res, next) =>{
         Promise.resolve(requestHandler(req, res, next)).
         catch((err) => next(err))
     }
