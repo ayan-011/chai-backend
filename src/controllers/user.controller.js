8258d9d3f7069b5ac1b469818d6b9a3febfba5bc
Your Name <ayansaifi2nd@gmail.com>
2024-10-25 18:33:52 +0530

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,19 +1,12 @@
-import { asyncHandler} from "../utils/asyncHandler.js"
+import { asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+
 
 const registerUser = asyncHandler( (req, res) =>{
-    
-    // 1. get user details from frontend.
-    // 2. validation - not empty.
-    // 3. check if user already exists: username, email.
-    // 4. check for images , check for avatar.
-    // 5. upload them to cloudinary, check avatar again.
-    // 6. create user object - create entry in db.
-    // 7. remove password and refresh token field from response.
-    // 8. check for user creation.
-    // 9. return response.
+
 
   const {fullName, email, username, password,  } = req.body
-  console.log("email:", email)
+  console.log("email:", email);
 
 })
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,14 @@ const router = Router()
 
 router.route("/register").post(
     upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
         
     ]),
     registerUser)
